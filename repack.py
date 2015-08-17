65e982b510067153eb496550136ce28d25ebb09a
Hrimfaxi <outmatch@gmail.com>
2015-08-17 21:07:37 +0000

    添加注释并修复repack.py错误

--- repack.py
+++ repack.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# coding: utf-8
+
 import sys
 import os
 import ftplib
@@ -6,38 +8,51 @@ import glob
 import struct
 import codecs
 
-
-
+# 原文
 fin = codecs.open('1.txt','r',encoding='utf-8');
+# 翻译列表，格式为开始偏移量，结束偏移，原文，翻译
 fincsv = codecs.open('all.csv', 'r', encoding='utf-8');
+# 输出
 fout = codecs.open('final.csv', 'w', encoding='utf-8');
+# 原文的全部内容
 csv = fin.read();
-
 
 fix = 0;
 cnt = 0;
+# 读入一行
 line = fincsv.readline();
+# 最后的位置
 lastpos = 0;
 
-
 while (line):
+	# 使用逗号分割
 	arr = line.strip().split(',');
-	if ((len(arr) == 3) or ((len(arr) == 4) and (arr[4] == ''))):
+	if len(arr) == 4:
+		# 开始位置
 		pos0 = int(arr[0]);
+		# 结束位置
 		pos1 = int(arr[1]);
-		txt = (arr[2]);
-		fout.write( csv[lastpos:pos0] + txt);
+		# 翻译后的文本
+		txt = (arr[3]);
+		# 写翻译位置之前的文本
+		fout.write(csv[lastpos:pos0] + txt);
+		# 更新最后位置
 		lastpos = pos1;
 	else:
+		# 报错
 		print('error at: ' );
-		print(line);
-
+		print(line)
+		assert(False)
 		
+	# 记数+1
 	cnt += 1;
+	# 每10000个更新打印
 	if (cnt % 10000 == 0):
 		print(cnt);
-
 		
+	# 读下一行
 	line = fincsv.readline();
 
+# 写最后的
 fout.write(csv[lastpos:]);
+print ("%d line imported" % (cnt))
--- unpack.py
+++ unpack.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# coding: utf-8
+
 import sys
 import os
 import ftplib
@@ -6,10 +8,13 @@ import glob
 import struct
 import codecs
 
+# 导出脚本
 
-
+# 原文/CSV格式
 fin = codecs.open('1.txt','r',encoding='utf-8');
+# 输出CSV文件 (转化为人类可读形式)
 fout = codecs.open('out.csv', 'w', encoding='utf-8');
+# 得到整个原文
 origcsv = fin.read();
 lastWide = False;
 wdstr = u'';
@@ -18,29 +23,45 @@ wdend = 0;
 
 i = 0;
 
+# 保留的字符串
 reservedStr = '<%=name%>';
 
 while(i < len(origcsv)):
 	ch = origcsv[i];
+	# 是汉字？
 	isWide = ord(ch) > 127;
 	if (isWide) :
+		# 上一个不是汉字
 		if (not lastWide) :
+			# 记录字符串
 			wdstr = ch;
+			# 记录下偏移量
 			wdstart = i;
 		else :
 			wdstr += ch;
-	else: 
+	else:
+		# 不是汉字
+		# 上一个是汉字？
 		if (lastWide) :
+			# 是保留字？
 			if (origcsv[i:i + len(reservedStr)] == reservedStr) :
+				# 加入保留字长度到偏移量
 				i += len(reservedStr);
+				# 加入保留字到wdstr
 				wdstr += reservedStr;
+				# 继续
 				continue;
 				
+			# 记录结束字
 			wdend = i;
+			# 输出格式: 开始偏移，结束偏移，字符串
 			r = u'';
 			r += str(wdstart) + ',' + str(wdend) + ',';
 			r += wdstr;
 			r += '\r\n';
+			# 输出
 			fout.write(r);
+	# 记录上一个字符是否为汉字
 	lastWide = isWide;
+	# 下一个字符
 	i += 1;
