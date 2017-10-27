a69a974cfc89fe3c90b10de7c95bb5e15bc6fc38
Hrimfaxi <outmatch@gmail.com>
2017-10-27 21:43:12 +0800

    added unique mode and fixed an invalid assert
    Signed-off-by: Hrimfaxi <outmatch@gmail.com>

--- unpack.py
+++ unpack.py
@@ -36,10 +36,15 @@ def isHanzi(ch):
 	return ord(ch) >= 0x80
 
 def main():
+	transDict = {}
 	argparser = argparse.ArgumentParser('unpack')
 	argparser.add_argument('input')
 	argparser.add_argument('output')
+	argparser.add_argument('--unique', '-u', action='store_true')
 	args = argparser.parse_args()
+
+	if args.unique:
+		print ("Unique mode selected")
 
 	with open(args.input, "r", encoding='utf-8') as fi:
 		inp = fi.read()
@@ -48,13 +53,9 @@ def main():
 		caught_hanzi = False
 		entries = []
 		string = ""
-		cjk = 0
 
 		while curr < len(inp):
 			ch = inp[curr]
-			if isCJK(ch):
-				cjk += 1
-				# print(ch, end='')
 
 			if isHanzi(ch):
 				string += ch
@@ -73,23 +74,35 @@ def main():
 					caught_hanzi = False
 					end = curr
 
-					# print (begin, end, string)
-					entries.append(TransEntry(begin, end, string))
+					if args.unique:
+						if string in transDict:
+							pass
+						else:
+							#print (begin, end, string)
+							entries.append(TransEntry(begin, end, string))
+							transDict[string] = True
+					else:
+						#print (begin, end, string)
+						entries.append(TransEntry(begin, end, string))
 					string = ""
 			curr += 1
 
 		curr = 0
 		for e in entries:
-			assert(curr < e.begin)
+			assert(curr <= e.begin)
 			assert(e.begin < e.end)
 			assert(inp[e.begin:e.end] == e.string)
 			curr = e.end
 			# print (e.begin, e.end, e.string)
 
+	cjk = 0
 	with open(args.output, "w", encoding='utf-8') as fo:
 		csv_writer = csv.writer(fo)
 		for e in entries:
 			csv_writer.writerow([e.begin, e.end, e.string])
+			for ch in e.string:
+				if isCJK(ch):
+					cjk += 1
 	print ("{}: {} entries exported. {} CJK charactors found.".format(args.output, len(entries), cjk))
 
 if __name__ == "__main__":
